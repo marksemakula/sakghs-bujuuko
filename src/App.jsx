c0c68d672c0de8e464577404d65df64870906463
Mark SEMAKULA™ <marksemakula@gmail.com>
2026-03-03 03:35:40 +0300

    Files Upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import SAKGHSBujuuko from './pages/SAKGHSBujuuko';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <SAKGHSBujuuko />
+    </BrowserRouter>
+  );
+}
+
+export default App;
